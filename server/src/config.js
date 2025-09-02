d12f671bd75c48408736fc94ffdede2f64d7b3f9
Felister Paul <felisterpaul4@gmail.com>
2025-09-02 13:18:00 +0300

    added

--- server/src/config.js
+++ server/src/config.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 5000,
-  mongoUri: 'mongodb://localhost:27017/qa-portfolio',
-  corsOrigin: 'http://localhost:5173'
+  mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/qa-portfolio',
+  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173'
 };
