efcbcf7ac2a35864b88546eabf047a3bf94f93b0
Felister Paul <felisterpaul4@gmail.com>
2025-09-08 13:39:38 +0300

    change login button

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -53,7 +53,7 @@ export default function Login() {
           <div>
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-900 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             >
               Login
             </button>
